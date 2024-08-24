dd568206e4575056502e063b190e916d1f5f999c
nimaste <nicolasvalsi203@gmail.com>
2024-08-23 21:49:10 -0300

    os

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import render_template, Flask, request
 import ChatBot as cb
+import os
 app = Flask(__name__)
 
 @app.route('/')
@@ -13,4 +14,5 @@ def home():
     return render_template('index.html',resposta=resposta)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
